06919c26f61b42463937dfa17717b734c55b8019
Mario Diaz <mariodiar@gmail.com>
2016-11-01 18:34:09 -0600

    Add single-criteria-selector component that contains the toggle and slider

--- frontend/app/components/criteria-picker.js
+++ frontend/app/components/criteria-picker.js
@@ -1,4 +1,20 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+	isVariablesOn: false,
+	isFunctionsOn: false,
+	isConstantsOn: false,
+	isCommentsInsideOn: false,
+	isCommentsBeforeOn: false,
+	isFilenameOn: false,
+	isLibrariesOn: false,
+
+	totalPercentage: 0,
+
+
+	actions: {
+		testValues() {
+			console.log(this.get('isVariablesOn'));
+		}
+	}
 });
