7a2fafba9ad845a98ab9cc239cd0e10663561037
Mario Diaz <mariodiar@gmail.com>
2016-11-01 19:23:42 -0600

    Add totalPercentage selected on right column

--- frontend/app/controllers/selection.js
+++ frontend/app/controllers/selection.js
@@ -1,5 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-	openCriteriaPicker: true
+	openCriteriaPicker: true,
+
+	actions: {
+		OpenCriteriaPicker() {
+			if(this.get('openCriteriaPicker')) {
+				this.set('openCriteriaPicker', false);
+			} else {
+				this.set('openCriteriaPicker', true);
+			}
+		}
+	}
 });
