c9f90713bd99b0b2c42d770b024055acef82a05f
Mario Diaz <mariodiar@gmail.com>
2016-11-01 18:47:00 -0600

    Add totalPercentage calculated automatically by the sum of all slider values

--- frontend/app/components/criteria-picker.js
+++ frontend/app/components/criteria-picker.js
@@ -2,14 +2,31 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
 	isVariablesOn: false,
-	isFunctionsOn: false,
-	isConstantsOn: false,
-	isCommentsInsideOn: false,
-	isCommentsBeforeOn: false,
-	isFilenameOn: false,
-	isLibrariesOn: false,
+	variablesValue: 0,
 
-	totalPercentage: 0,
+	isFunctionsOn: false,
+	functionsValue: 0,
+
+	isConstantsOn: false,
+	constantsValue: 0,
+
+	isCommentsInsideOn: false,
+	commentsInsideValue: 0,
+
+	isCommentsBeforeOn: false,
+	commentsBeforeValue: 0,
+
+	isFilenameOn: false,
+	filenameValue: 0,
+
+	isLibrariesOn: false,
+	librariesValue: 0,
+
+	totalPercentage: Ember.computed('variablesValue', 'functionsValue', 'constantsValue', 'commentsInsideValue', 
+					'commentsBeforeValue','filenameValue', 'librariesValue', function() {
+		return this.get('variablesValue') + this.get('functionsValue') + this.get('constantsValue') + 
+				this.get('commentsBeforeValue') + this.get('commentsInsideValue') + this.get('filenameValue') + this.get('librariesValue');
+	}),
 
 
 	actions: {
