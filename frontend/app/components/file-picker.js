0164a58285c46a770f4b4f566019260d54d27162
Mario Diaz <mariodiar@gmail.com>
2016-11-25 14:54:12 -0600

    Remove files by passing index instead of object

--- frontend/app/components/file-picker.js
+++ frontend/app/components/file-picker.js
@@ -33,8 +33,10 @@ export default Ember.Component.extend({
   	},
 
   	actions: {
-  		removeFile() {
-  			this.get('files').removeObject(file);
+  		removeFile(index) {
+			console.log(index);
+  			this.get('files').removeAt(index);
+			console.log(this.get('files'));
   		},
 
   		openSingleFileSelector() {
@@ -70,68 +72,3 @@ export default Ember.Component.extend({
 		}
   	}
 });
-
-// attachFiles(files, ticket, user, email) {
-// 			var userId = null;
-
-// 			if(Ember.isEmpty(user))
-// 				userId = null;
-// 			else
-// 				userId = user.id;
-
-// 			const uploader = EmberUploader.Uploader.create({
-// 				url: 'api/attachments',
-// 				paramName: 'attachment_path'
-// 			});
-
-// 			uploader.on('progress', () => {
-// 				this.set('isUploading', true);
-// 				return this.set('attachmentError', '');
-// 			});
-
-// 			uploader.on('didUpload', res => {
-// 				this.store.pushPayload(res);
-// 				return this.set('isUploading', false);
-// 			});
-
-// 			uploader.on('didError', (jqXHR, textStatus, errorThrown) => {
-// 				this.set('attachmentError', errorThrown);
-// 				return this.set('isUploading', false);
-// 			});
-
-// 			if (!Ember.isEmpty(files)) {
-// 				for(var i = 0; i < files.length; ++i)
-// 				uploader.upload(files[i], {
-// 					"ticket_id": ticket.id,
-// 					"visibility": true,
-// 					"user_id": userId,
-// 					"email": email
-// 				});
-// 			}
-// 		}
-
-// setFiles() {
-//     	if(this.get('files') === null) {
-//     		this.set('filesAreSetted', true);
-//     		this.set('files', document.getElementById('request-attachments').files);
-//     	} else {
-//     		var myFiles = this.get('files');
-//     		var newFiles = document.getElementById('request-attachments').files;
-//     		var totalFiles = [];
-//     		var  i;
-
-//     		totalFiles.length = myFiles.length + newFiles.length;
-
-//     		for(i = 0; i < myFiles.length; ++i){
-//     			totalFiles[i] = myFiles[i];
-//     		}
-
-//     		for(var j = 0; j < newFiles.length; ++j){
-//     			totalFiles[i] = newFiles[j];
-//     			++i;
-//     		}
-
-//     		this.set('files', totalFiles);
-//     	}
-//     },
-
