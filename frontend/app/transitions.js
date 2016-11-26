5b304829197a39dcb57216c51948d0698c61bf9c
Mario Diaz <mariodiar@gmail.com>
2016-11-26 04:36:03 -0600

    Add transition to criteriaPicker aparition

--- frontend/app/transitions.js
+++ frontend/app/transitions.js
@@ -16,6 +16,20 @@ export default function() {
         this.reverse('crossFade', {durationFilepicker})
     );
 
+    let durationCriteria = 2000;
+    this.transition(
+        this.hasClass('criteriaTest'),
+
+        // this makes our rule apply when the liquid-if transitions to the
+        // true state.
+        this.toValue(true),
+        this.use('crossFade', {durationCriteria}),
+
+        // which means we can also apply a reverse rule for transitions to
+        // the false state.
+        this.reverse('crossFade', {durationCriteria})
+    );
+
     let durationCriteriaBanner = 3000;
     this.transition(
         this.hasClass('criteriaBanner'),
