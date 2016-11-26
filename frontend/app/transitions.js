a9d25612d230550bb00c1b260ebc5caf0056bb68
Mario Diaz <mariodiar@gmail.com>
2016-11-26 04:29:11 -0600

    Improve transition of filePicker

--- frontend/app/transitions.js
+++ frontend/app/transitions.js
@@ -1,7 +1,7 @@
 import Ember from "ember";
 
 export default function() {
-    let duration = 1000;
+    let durationFilepicker = 1000;
 
       this.transition(
         this.hasClass('filePicker'),
@@ -9,10 +9,24 @@ export default function() {
         // this makes our rule apply when the liquid-if transitions to the
         // true state.
         this.toValue(true),
-        this.use('crossFade', {duration}),
+        this.use('crossFade', {durationFilepicker}),
 
         // which means we can also apply a reverse rule for transitions to
         // the false state.
-        this.reverse('crossFade', {duration})
+        this.reverse('crossFade', {durationFilepicker})
+    );
+
+    let durationCriteriaBanner = 3000;
+    this.transition(
+        this.hasClass('criteriaBanner'),
+
+        // this makes our rule apply when the liquid-if transitions to the
+        // true state.
+        this.toValue(true),
+        this.use('toLeft', {durationCriteriaBanner}),
+
+        // which means we can also apply a reverse rule for transitions to
+        // the false state.
+        this.reverse('toLeft', {durationCriteriaBanner})
     );
 };
